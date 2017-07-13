a7c6f348c741d4a7e528f381f33524a44b0aa1ce
ndxbxrme <lewis_the_cat@hotmail.com>
2017-07-13 23:34:36 +0100

    first commit

---
+++ build/index.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+  module.exports = function(ndx) {
+    ndx.app.get('/api/impersonate/:userId', ndx.authenticate(['superadmin', 'admin']), function(req, res, next) {
+      res.cookie('impersonate', ndx.generateToken(ndx.user[ndx.settings.AUTO_ID], null), {
+        maxAge: 7 * 24 * 60 * 60 * 1000
+      });
+      ndx.user[ndx.settings.AUTO_ID] = req.params.userId;
+      ndx.setAuthCookie(req, res);
+      return res.redirect('/');
+    });
+    return ndx.app.get('/api/unimpersonate', function(req, res, next) {
+      ndx.user[ndx.settings.AUTO_ID] = ndx.parseToken(req.cookies.impersonate);
+      res.clearCookie('impersonate');
+      ndx.setAuthCookie(req, res);
+      return res.redirect('/');
+    });
+  };
+
+}).call(this);
+
+//# sourceMappingURL=index.js.map
---
+++ src/index.coffee
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = (ndx) ->
+  ndx.app.get '/api/impersonate/:userId', ndx.authenticate(['superadmin', 'admin']), (req, res, next) ->
+    res.cookie 'impersonate', ndx.generateToken(ndx.user[ndx.settings.AUTO_ID], null), maxAge: 7 * 24 * 60 * 60 * 1000
+    ndx.user[ndx.settings.AUTO_ID] = req.params.userId
+    ndx.setAuthCookie req, res
+    res.redirect '/'
+  ndx.app.get '/api/unimpersonate', (req, res, next) ->
+    ndx.user[ndx.settings.AUTO_ID] = ndx.parseToken req.cookies.impersonate
+    res.clearCookie 'impersonate'
+    ndx.setAuthCookie req, res
+    res.redirect '/'
